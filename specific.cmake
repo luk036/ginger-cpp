5ea17cfec2f1bc4f882b8f034bc69eb3b8b5f91f
Wai-Shing Luk <luk036@gmail.com>
2024-11-01 01:17:37 +0000

    update lds-gen 1.0.4

--- specific.cmake
+++ specific.cmake
@@ -10,7 +10,7 @@ find_package(Threads REQUIRED)
 
 CPMAddPackage(
   NAME LdsGen
-  GIT_TAG 1.0.3
+  GIT_TAG 1.0.4
   GITHUB_REPOSITORY luk036/lds-gen-cpp
   OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
