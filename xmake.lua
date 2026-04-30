ea7df312a2901ef3a24dbcb814562319fe092cc3
Wai-Shing Luk <luk036@gmail.com>
2026-04-30 19:26:46 +0800

    upgrade fmt 12.1.0 and spdlog 1.17.0

--- xmake.lua
+++ xmake.lua
@@ -21,7 +21,7 @@ if is_plat("linux") then
 		add_sysincludedirs(termux_prefix .. "/include", { public = true })
 	end
 elseif is_plat("windows") then
-	add_cxflags("/EHsc /W4 /WX", { force = true })
+	add_cxflags("/EHsc /W4 /WX /wd4459", { force = true })
 	add_ldflags("/FORCE:MULTIPLE", { force = true })
 	add_defines("_SILENCE_CXX17_RESULT_OF_DEPRECATION_WARNING")
 end
